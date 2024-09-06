cf357850a0c932556199ef9e57289af8cdc12cbe
Aditya-Garg-01 <142223240+Aditya-Garg-01@users.noreply.github.com>
2024-09-06 18:49:02 +0530

    Update indexs.js

--- indexs.js
+++ indexs.js
@@ -1,7 +1,7 @@
 var randomnumber1;
 var randomnumber2;
 document.querySelector(".b1").addEventListener("click", function () {
-  randomnumber1 = Math.floor(Math.random() * 6);
+  randomnumber1 = Math.floor(Math.random() * 6)+1;
   console.log(randomnumber1);
   var dice1img = "./images/dice" + randomnumber1 + ".png";
   document.querySelector(".img1").setAttribute("src", dice1img);
